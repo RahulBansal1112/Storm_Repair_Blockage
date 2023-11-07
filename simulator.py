59993024fd52c28df962bae5d86fb33ff0c59af9
RahulBansal <rahulbansal2004@gmail.com>
2023-11-07 17:52:24 -0600

    Fixed starter template for simulation

--- simulator.py
+++ simulator.py
@@ -5,18 +5,31 @@ class Simulator:
     """Simulates multiple agents traversing a graph with multiple target
 
     Attributes:
-        num_nodes: number of nodes from [0, n)
-        adjacen_list: directed adjacency list
-        edge_weight: 2D matrix representing l(i, j)
-        node_weight: list of all weights w(i)
-
+        TODO
     """
+
+    
 
     def __init__(self, known: Graph, unknown: Graph, visibility: List[List[Tuple]], num_agents: int, targets: List[int]):
         pass
 
-    def start_sim(algorithm: str, start_nodes: List[int]):
+    def start_sim(self, algorithm: str, start_nodes: List[int]):
         pass
+
+    def run_simulation(self):
+        # while(True):
+        #     _update_positions(self)
+        pass
+
+    def _update_positions(self) -> int:
+        time_delta = min(self.known.edge_weight[self.agent_pos[agent]][self.agent_dest[agent]] - self.agent_progress[agent] for agent in range(self.num_agents))
+        self.agent_progress = [progress + time_delta for progress in self.agent_progress]
+        for agent in range(self.num_agents):
+            if self.known.edge_weight[self.agent_pos[agent]][self.agent_dest[agent]] == self.agent_progress[agent]:
+                self.agent_pos[agent] = self.agent_dest[agent]
+                self.agent_dest[agent] = -1
+                self.agent_progress[agent] = 0
+        return time_delta
+
+
     
-    def run_simulation():
-        pass
