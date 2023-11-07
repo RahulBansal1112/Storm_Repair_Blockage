98385a2b2ff5fa4749b7facc031eed6c9a12de2f
RahulBansal <rahulbansal2004@gmail.com>
2023-11-07 17:00:03 -0600

    Simplified graph methods and created Simulator template

---
+++ simulator.py
@@ -0,0 +1,22 @@
+from graph import Graph
+from typing import List, Tuple
+
+class Simulator:
+    """Simulates multiple agents traversing a graph with multiple target
+
+    Attributes:
+        num_nodes: number of nodes from [0, n)
+        adjacen_list: directed adjacency list
+        edge_weight: 2D matrix representing l(i, j)
+        node_weight: list of all weights w(i)
+
+    """
+
+    def __init__(self, known: Graph, unknown: Graph, visibility: List[List[Tuple]], num_agents: int, targets: List[int]):
+        pass
+
+    def start_sim(algorithm: str, start_nodes: List[int]):
+        pass
+    
+    def run_simulation():
+        pass
