a9ae11df748c42836905e381f4405a2d6a7bf8ea
RahulBansal <rahulbansal2004@gmail.com>
2023-11-07 18:03:27 -0600

    Created methods to update the position and known graph in the simulator

--- simulator.py
+++ simulator.py
@@ -8,7 +8,13 @@ class Simulator:
         TODO
     """
 
-    
+    # agent_pos = []
+    # agent_dest = []
+    # agent_progress = []
+    # num_agents = 0
+    # known: Graph
+    # time = 0
+    # visibility = []
 
     def __init__(self, known: Graph, unknown: Graph, visibility: List[List[Tuple]], num_agents: int, targets: List[int]):
         pass
@@ -17,9 +23,11 @@ class Simulator:
         pass
 
     def run_simulation(self):
-        # while(True):
-        #     _update_positions(self)
-        pass
+        while(len(self.targets) > 0):
+            self._update_positions()
+            self._update_known_graph()
+            # self._get_new_dest()
+        
 
     def _update_positions(self) -> int:
         time_delta = min(self.known.edge_weight[self.agent_pos[agent]][self.agent_dest[agent]] - self.agent_progress[agent] for agent in range(self.num_agents))
@@ -30,6 +38,14 @@ class Simulator:
                 self.agent_dest[agent] = -1
                 self.agent_progress[agent] = 0
         return time_delta
+    
+    def _update_known_graph(self) -> None:
+        for agent in range(self.num_agents):
+            if self.agent_progress[agent] == 0:
+                visible_edges = self.visibility[self.agent_pos[agent]]
+                for edge in visible_edges:
+                    self.known.set_edge_weight(edge[0], edge[1], self.unknown.edge_weight[edge[0]][edge[1]]) # WILL PROBABLY NEED TO DELETE EDGE
+
 
 
     
