a5b29c4fe61bace4cabaae7d0dd3040268a7dd1f
Rohan <rohansb2@illinois.edu>
2024-10-21 20:52:04 -0500

    test

--- graph_generator.py
+++ graph_generator.py
@@ -1,14 +1,25 @@
 from graph import Graph, graph_dict
 import random
 
-graph_node_range = 0
-graph_edge_range = 0
-graph_node_weight_range = 0
+graph_node_range = 0 #set this for number of nodes you want
+graph_edge_range = 0 #set this for number of edges you want (edges must be at least the same as number of nodes)
+'''
+edges must be greater than (n-1) and less than (n)(n-1)
+
+'''
+graph_node_weight_range = 0 
 graph_node_chance = 0
 
-assert (graph_node_range < graph_edge_range)
+assert (graph_edge_range > (graph_node_range - 1) and graph_edge_range <= (graph_node_range - 1)*(graph_node_range))
+
+'''
+steps:
+create known graph -> ensure that every node is connected
+use known graph to create corresponding "unknown" graph -> ensure every node is connected
 
 
+
+'''
 
 random_graph_dict: graph_dict = {
    "num_nodes": 0,
